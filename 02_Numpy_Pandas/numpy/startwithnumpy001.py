815ca11d252c3e27679487d37b46fed483de2304
Ankitgujare <ankitgujare008@gmail.com>
2026-02-28 01:11:38 +0530

    Day 4 starting Numpy

---
+++ 02_Numpy_Pandas/numpy/startwithnumpy001.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+number=[1,2,3,4,5]
+arr=np.array(number)
+print(arr,type(arr))
+
+
+"""" 
+we have created 1D array 
+we can aslo create nd arrays 
+lets see how we can do that
+
+"""
